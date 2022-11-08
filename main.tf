5f5507f375ae887ad347962c28a219f9f75ef1c2
dkasyan <damian.kasyan@hotmail.com>
2022-11-08 09:54:06 +0100

    Interpolation

--- main.tf
+++ main.tf
@@ -2,6 +2,23 @@ provider "aws" {
     region = "eu-west-1"
 }
 
-resource "aws_vpc" "myVPC" {
+variable "cloud_name" {
+    type = string
+    default = "Chmura_testowa"
+}
+
+variable "input_name" {
+    type = string
+    description = "Set the name of project"
+  
+}
+
+
+#Do czego słuźy zmienna vpc_name. Jest niezbędna, ale jej nazwa nie jest nigdzie wpisywana
+resource "aws_vpc" "vpc_name" {
     cidr_block = "10.0.0.0/16"
+    tags = {
+        terraform = "true"
+        Name = "VPC_${var.cloud_name}"
+}
 }
