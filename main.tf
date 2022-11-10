dab338d0b591a552be281e0de6b3cb4f0b7d9a0c
dkasyan <damian.kasyan@hotmail.com>
2022-11-10 16:56:51 +0100

    destroy

--- main.tf
+++ main.tf
@@ -63,8 +63,8 @@ resource "aws_security_group" "webtraffic" {
 ### Lambdy ###
 
 resource "aws_iam_role" "lambda_role" {
-name   = "Spacelift_Test_Lambda_Function_Role"
-assume_role_policy = <<EOF
+  name = "Spacelift_Test_Lambda_Function_Role"
+  assume_role_policy = <<EOF
 {
  "Version": "2012-10-17",
  "Statement": [
@@ -83,9 +83,9 @@ EOF
 
 resource "aws_iam_policy" "iam_policy_for_lambda" {
  
- name         = "aws_iam_policy_for_terraform_aws_lambda_role"
- path         = "/"
- description  = "AWS IAM Policy for managing aws lambda role"
+ name = "aws_iam_policy_for_terraform_aws_lambda_role"
+ path = "/"
+ description = "AWS IAM Policy for managing aws lambda role"
  policy = <<EOF
 {
  "Version": "2012-10-17",
@@ -105,21 +105,21 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
- role        = aws_iam_role.lambda_role.name
- policy_arn  = aws_iam_policy.iam_policy_for_lambda.arn
+  role = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.iam_policy_for_lambda.arn
 }
 
 data "archive_file" "zip_the_python_code" {
-type        = "zip"
-source_dir  = "${path.module}/python/"
-output_path = "${path.module}/python/hello-python.zip"
+  type = "zip"
+  source_dir = "${path.module}/python/"
+  output_path = "${path.module}/python/hello-python.zip"
 }
 
 resource "aws_lambda_function" "terraform_lambda_func" {
-filename                       = "${path.module}/python/hello-python.zip"
-function_name                  = "Spacelift_Test_Lambda_Function"
-role                           = aws_iam_role.lambda_role.arn
-handler                        = "script.lambda_handler"
-runtime                        = "python3.9"
-depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+  filename = "${path.module}/python/hello-python.zip"
+  function_name = "Spacelift_Test_Lambda_Function"
+  role = aws_iam_role.lambda_role.arn
+  handler = "script.lambda_handler"
+  runtime = "python3.9"
+  depends_on = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
 }
